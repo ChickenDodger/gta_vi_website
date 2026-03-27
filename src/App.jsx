20ac74e912fe5fb007b2470c9e6204d7ff542ad9
ChickenDodger <LTLawson1@outlook.com>
2026-03-27 17:51:34 +0000

    Implemented Jason character info section, the second video & beginning of Lucia's character info section.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { ScrollTrigger } from 'gsap/all';
 import Hero from './sections/Hero';
 import NavBar from "./sections/NavBar.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
+import Jason from "./sections/Jason.jsx";
+import SecondVideo from "./sections/SecondVideo.jsx";
+import Lucia from "./sections/Lucia";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -14,6 +17,9 @@ const App = () => {
                 <NavBar />
                 <Hero />
                 <FirstVideo />
+                <Jason />
+                <SecondVideo />
+                <Lucia />
             </div>
         </main>
     )
