f047ac33e69d663663b91cc8351498f5725a0e3c
ChickenDodger <LTLawson1@outlook.com>
2026-03-28 18:35:54 +0000

    Implemented Lucia section

--- src/sections/Lucia.jsx
+++ src/sections/Lucia.jsx
@@ -2,7 +2,27 @@ import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
 
 const Lucia = () => {
+    useGSAP(() => {
+        gsap.set('.lucia-life', { marginTop: '-80vh' });
 
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '.lucia-life',
+                start: 'top 80%',
+                end: '10% center',
+                scrub: 2,
+            }
+        }).to('.second-vd', { opacity: 0, duration: 1, ease: 'power1.inOut' });
+
+        gsap.to('.lucia-life .img-box', {
+            scrollTrigger: {
+                trigger: '.lucia-life',
+                start: 'top center',
+                end: '80% center',
+                scrub: 2
+            }, y: -200, duration: 1, ease: 'power1.inOut'
+        }, '<')
+    })
 
 
     return (
@@ -18,7 +38,7 @@ const Lucia = () => {
 
 
 
-        <div className=".lucia-life-content">
+        <div className="lg:w-1/2 lucia-life-content">
             <div className="max-w-xl lg:ps-32 ps-10">
                 <h1>Lucia Caminos</h1>
                 <h2>Lucia’s father taught her to fight as soon as she could walk.</h2>
