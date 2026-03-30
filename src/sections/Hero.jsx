3aced5297fae5b184f8964043f01abd548ab5d29
ChickenDodger <LTLawson1@outlook.com>
2026-03-30 01:53:50 +0100

    Fixing paths

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-import { useMaskSettings } from "../../Constants/index.js";
+import { useMaskSettings } from "../../Constants/";
 import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
