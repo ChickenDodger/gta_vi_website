b1763a98676fce0effab6d09faec9ddb54d011f2
ChickenDodger <LTLawson1@outlook.com>
2026-03-30 01:31:20 +0100

    Fixing casing for Vercel again

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,20 +1,21 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-//fix casing
-import { useMaskSettings } from '../../Constants';
+
+import { useMaskSettings } from '../../constants';
 import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
-    const {  initialMaskPos, initialMaskSize, maskSize} = useMaskSettings();
-
+    const { initialMaskPos, initialMaskSize, maskPos, maskSize } = useMaskSettings();
 
     useGSAP(() => {
-        gsap.set('.mask-wrapper', { maskPosition: initialMaskPos, maskSize: initialMaskSize });
+        gsap.set('.mask-wrapper', {
+            maskPosition: initialMaskPos,
+            maskSize: initialMaskSize,
+        });
 
+        gsap.set('.mask-logo', { marginTop: '-100vh', opacity: 0 });
 
-        gsap.set('.mask,logo', { marginTop: '-100vh', opacity: 0});
-
-        gsap.set('.entrance-message', { marginTop: '0vh'});
+        gsap.set('.entrance-message', { marginTop: '0vh' });
 
         const tl = gsap.timeline({
             scrollTrigger: {
@@ -29,8 +30,8 @@ const Hero = () => {
         tl
             .to('.fade-out', { opacity: 0, ease: 'power1.inOut' })
             .to('.scale-out', { scale: 1, ease: 'power1.inOut' })
-            .to( '.mask-wrapper',{ maskSize, ease: 'power1.inOut' }, '<')
-            .to('.mask-wrapper', { opacity: 0})
+            .to('.mask-wrapper', { maskSize, ease: 'power1.inOut' }, '<')
+            .to('.mask-wrapper', { opacity: 0 })
             .to('.overlay-logo', { opacity: 1, onComplete: () => {
                     gsap.to('.overlay-logo', { opacity: 0 });
                 } }, '<')
@@ -42,7 +43,7 @@ const Hero = () => {
             <div className="size-full mask-wrapper">
                 <img src="/images/hero-bg.webp" alt="background" className="scale-out" />
                 <img src="/images/hero-text.webp" alt="hero-logo" className="title-logo fade-out" />
-                <img src="/images/watch-trailer.png" alt="trailer" className="trailer-logo fade-out" />
+                <img  src="/images/watch-trailer.png" alt="trailer" className="trailer-logo fade-out" />
                 <div className="play-img fade-out">
                     <img src="/images/play.png" alt="play" className="w-7 ml-1" />
                 </div>
@@ -53,7 +54,7 @@ const Hero = () => {
             </div>
 
             <div className="fake-logo-wrapper">
-                <img src="/images/big-hero-text.svg"  alt = 'big-text' className="overlay-logo" />
+                <img src="/images/big-hero-text.svg" className="overlay-logo" alt="Big-Hero-text"/>
             </div>
 
             <ComingSoon />
