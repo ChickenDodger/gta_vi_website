03d09aa48180abe8d5c6f76705415fa1db2b116d
ChickenDodger <LTLawson1@outlook.com>
2026-03-30 01:32:44 +0100

    Fixing casing for Vercel again & constants

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
-import { useMaskSettings } from '../../constants';
+import { useMaskSettings } from '../../Constants';
 import ComingSoon from "./ComingSoon"
 
 const Hero = () => {
